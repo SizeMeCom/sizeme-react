e81536ead163ba845b7a136e950e743c3bcfac62
Teppo Jalava <tjjalava@iki.fi>
2017-03-11 00:59:18 +0200

    Linting settings

--- src/index.jsx
+++ src/index.jsx
@@ -1,16 +1,16 @@
-import React from 'react';
-import {render} from 'react-dom';
-import AwesomeComponent from './awesomecomponent.jsx'
+import React from "react";
+import {render} from "react-dom";
+import AwesomeComponent from "./awesomecomponent.jsx";
 
 class App extends React.Component {
-  render () {
-    return (
-    	<div>
-    		<p> Hello React! Toppen!</p>
-    		<AwesomeComponent />
-    	</div>
-    );
-  }
+    render() {
+        return (
+            <div>
+                <p> Hello React! Toppen!</p>
+                <AwesomeComponent />
+            </div>
+        );
+    }
 }
 
-render(<App/>, document.getElementById('app'));
+render(<App/>, document.getElementById("app"));
