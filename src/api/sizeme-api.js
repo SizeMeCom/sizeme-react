a5c579315931ff0f64f8e1a0726b49aff30c54a0
Teppo Jalava <tjjalava@iki.fi>
2017-03-16 00:50:45 +0200

    sizeme-api WIP

--- src/api/sizeme-api.js
+++ src/api/sizeme-api.js
@@ -1,44 +1,49 @@
-/* global ga */
+/* global ga, sizeme_options */
 import "./ga.js";
 
-class SizeMe {
-    constructor (contextAddress = "https://www.sizeme.com", gaTrackingID, pluginVersion = "UNKNOWN") {
-        console.log("Creating SizeMe..");
-        this.contextAddress = contextAddress;
-        this.pluginVersion = pluginVersion;
-        this._authToken = null;
+let contextAddress = sizeme_options.contextAddress || "https://www.sizeme.com";
+let gaTrackingId = sizeme_options.gaTrackingId;
+let pluginVersion = sizeme_options.pluginVersion;
 
-        let gaEnabled = false;
-        ga(function () {
-            gaEnabled = gaTrackingID != null;
-        });
-
-        this.trackEvent = (action, label) => {
-            if (gaEnabled) {
-                ga("create", gaTrackingID, "auto", { name: "sizemeTracker" });
-                this.trackEvent = (a, l) => {
-                    ga("sizemeTracker.send", {
-                        hitType: "event",
-                        eventCategory: window.location.hostname,
-                        eventAction: a,
-                        eventLabel: l
-                    });
-                };
-                this.trackEvent(action, label);
+let authToken = (() => {
+    let tokenObj = sessionStorage.getItem("sizeme.authtoken");
+    if (tokenObj) {
+        let storedToken;
+        try {
+            storedToken = JSON.parse(tokenObj);
+            if (storedToken.token && storedToken.expires && Date.parse(storedToken.expires) > new Date().getTime()) {
+                return storedToken.token;
             }
+        } catch (e) {
+            // no action
+        }
+    }
+    return null;
+})();
+
+let gaEnabled = false;
+ga(function () {
+    gaEnabled = gaTrackingId != null;
+});
+
+let trackEvent = (action, label) => {
+    if (gaEnabled) {
+        ga("create", gaTrackingId, "auto", { name: "sizemeTracker" });
+        trackEvent = (a, l) => {
+            ga("sizemeTracker.send", {
+                hitType: "event",
+                eventCategory: window.location.hostname,
+                eventAction: a,
+                eventLabel: l
+            });
         };
+        trackEvent(action, label);
     }
+};
 
-    set authToken (authToken) { this._authToken = authToken; }
 
-    isLoggedIn () {
-        return this._authToken != null;
-    }
+function isLoggedIn () {
+    return authToken != null;
 }
 
-export let sizeme;
-
-export const initSizeMe = (contextAddress, gaTrackingID, pluginVersion) => {
-    sizeme = new SizeMe(contextAddress, gaTrackingID, pluginVersion);
-    return sizeme;
-};
+export { trackEvent, isLoggedIn };
--- src/index.js
+++ src/index.js
@@ -4,13 +4,13 @@ import React from "react";
 import { render } from "react-dom";
 import { Provider } from "react-redux";
 import { createStore } from "redux";
+import { isLoggedIn } from "./api/sizeme-api";
 import sizeMeReducer from "./reducers/index";
-import { initSizeMe } from "./api/sizeme-api";
 import SizeMeApp from "./SizeMeApp.jsx";
 
-initSizeMe(sizeme_options.contextAddress, undefined, sizeme_options.pluginVersion);
-
-let store = createStore(sizeMeReducer);
+let store = createStore(sizeMeReducer, {
+    loggedIn: isLoggedIn()
+});
 
 let section = document.createElement("div");
 document.getElementById("product-options-wrapper").appendChild(section);
