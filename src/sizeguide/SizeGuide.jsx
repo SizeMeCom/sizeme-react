0f8a329d910204108f240e7ea9f42b5d23a4b201
Teppo Jalava <tjjalava@iki.fi>
2017-03-21 01:39:42 +0200

    Size guide start, stylesheet configurations

--- src/sizeguide/SizeGuide.jsx
+++ src/sizeguide/SizeGuide.jsx
@@ -1,11 +1,37 @@
 import React from "react";
+import Modal from "react-modal";
 
 class SizeGuide extends React.Component {
+    constructor (props) {
+        super(props);
+
+        this.state = {
+            guideIsOpen: false
+        };
+    }
+
+    openGuide = () => {
+        this.setState({ guideIsOpen: true });
+    };
+
+    closeGuide = () => {
+        this.setState({ guideIsOpen: false });
+    };
 
     render () {
         return (
             <div>
-                <a className="a_button sm_detailed_view size_guide" id="popup_opener">Size guide</a>
+                <a className="a_button sm_detailed_view size_guide" id="popup_opener"
+                   onClick={this.openGuide}>Size guide</a>
+                <Modal
+                    isOpen={this.state.guideIsOpen}
+                    onRequestClose={this.closeGuide}
+                    contentLabel="Size Guide"
+                    className="size-guide-content"
+                    overlayClassName="size-guide-overlay"
+                >
+                    <h2>Hello</h2>
+                </Modal>
             </div>
         );
     }
--- webpack.config.js
+++ webpack.config.js
@@ -2,13 +2,16 @@
 
 const path = require("path");
 const webpack = require("webpack");
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 const BUILD_DIR = path.resolve(__dirname, "dist");
 const APP_DIR = path.resolve(__dirname, "src");
+const SCSS_DIR = path.join(APP_DIR, "scss");
 
 module.exports = {
     entry: {
-        main: APP_DIR + "/index.js"
+        "sizeme": `${APP_DIR}/index.js`,
+        "sizeme-styles": `${SCSS_DIR}/main.scss`
     },
     output: {
         path: BUILD_DIR,
@@ -21,14 +24,18 @@ module.exports = {
             filename: "[file].map"
         }),
         new webpack.optimize.CommonsChunkPlugin({
-            name: "vendor",
+            name: "sizeme-vendor",
             minChunks: function (module) {
                 return module.context && module.context.indexOf("node_modules") !== -1;
             }
         }),
         new webpack.optimize.CommonsChunkPlugin({
-            name: "manifest",
+            name: "sizeme-manifest",
             minChunks: Infinity
+        }),
+        new ExtractTextPlugin({
+            filename: "[name].css",
+            allChunks: true
         })
     ],
     module: {
@@ -37,6 +44,16 @@ module.exports = {
                 test: /\.jsx?/,
                 include: APP_DIR,
                 use: "babel-loader"
+            },
+            {
+                test: /\.scss$/,
+                include: SCSS_DIR,
+                loader: ExtractTextPlugin.extract(["css-loader", "sass-loader"])
+            },
+            {
+                test: /\.(png|jpg)$/,
+                include: path.join(APP_DIR, "images"),
+                loader: "url-loader?limit=10000"
             }
         ]
     }
