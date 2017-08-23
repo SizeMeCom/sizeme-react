21e0738f8c9507d376f7941a7c9d2eb0374341b3
Teppo Jalava <tjjalava@iki.fi>
2017-08-23 10:13:56 +0300

    Add check to see if ga is already loaded

--- src/api/ga.js
+++ src/api/ga.js
@@ -1,20 +1,22 @@
 /* global ga, sizeme_options */
-(function (i, s, o, g, r) {
-    i["GoogleAnalyticsObject"] = r;
-    i[r] = i[r] ||
-        function () {
-            (i[r].q = i[r].q || []).push(arguments);
-        };
-    i[r].l = 1 * new Date();
-    let a = s.createElement(o);
-    let m = s.getElementsByTagName(o)[0];
-    a.async = 1;
-    a.src = g;
-    m.parentNode.insertBefore(a, m);
-})(window, document, "script", "https://www.google-analytics.com/analytics.js", "ga");
-
 let gaTrackingId = sizeme_options.gaTrackingId;
 let gaEnabled = gaTrackingId !== null;
+
+if (gaEnabled && !ga) {
+    (function (i, s, o, g, r) {
+        i["GoogleAnalyticsObject"] = r;
+        i[r] = i[r] ||
+            function () {
+                (i[r].q = i[r].q || []).push(arguments);
+            };
+        i[r].l = 1 * new Date();
+        let a = s.createElement(o);
+        let m = s.getElementsByTagName(o)[0];
+        a.async = 1;
+        a.src = g;
+        m.parentNode.insertBefore(a, m);
+    })(window, document, "script", "https://www.google-analytics.com/analytics.js", "ga");
+}
 
 let trackEvent = (action, label) => {
     if (gaEnabled) {
