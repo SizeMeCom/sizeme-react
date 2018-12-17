e6741d130732988def98a80444c390c4053bc51e
Teppo Jalava <tjjalava@iki.fi>
2018-12-17 22:03:53 +0200

    Dispatch a "change"-event from the default, select-based size selector so other listeners also notice the change

--- webpack.config.js
+++ webpack.config.js
@@ -70,7 +70,8 @@ const developmentConfig = merge([
     parts.page({ template: "kookenka2.html", filename: "kookenka2.html" }),
     parts.page({ template: "selectric.html", filename: "selectric.html" }),
     parts.page({ template: "makia1.html", filename: "makia1.html" }),
-    parts.page({ template: "harrysoflondon.html", filename: "harrysoflondon.html" })
+    parts.page({ template: "harrysoflondon.html", filename: "harrysoflondon.html" }),
+    parts.page({ template: "levelshoes.html", filename: "levelshoes.html" })
 ]);
 
 const productionConfig = merge([
