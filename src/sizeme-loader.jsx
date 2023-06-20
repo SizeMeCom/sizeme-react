2184050e79c7c4763f49c04a716b67a7a8f9016b
Teppo Jalava <tjjalava@iki.fi>
2023-06-21 00:32:56 +0300

    React 18 upgrade

--- src/sizeme-loader.jsx
+++ src/sizeme-loader.jsx
@@ -1,11 +1,10 @@
 import { selectSize, setSizemeHidden, sizemeStore } from "./api/sizeme-api";
 import uiOptions from "./api/uiOptions";
-import { render } from "react-dom";
+import { createRoot } from "react-dom/client";
 import { I18nextProvider } from "react-i18next";
 import i18n from "./i18n";
 import { Provider } from "react-redux";
 import SizeSelector from "./api/SizeSelector";
-import React from "react";
 import "./scss/index.scss";
 import SizeMeAppWrapper from "./SizeMeAppWrapper";
 import { findVisibleElement } from "./common/utils";
@@ -19,13 +18,14 @@ const el = findVisibleElement(uiOptions.appendContentTo);
 
 if (el) {
   const section = el.appendChild(document.createElement("div"));
-  render(
+  const root = createRoot(section);
+  root.render(
     <I18nextProvider i18n={i18n}>
       <Provider store={sizemeStore}>
         <SizeMeAppWrapper />
       </Provider>
-    </I18nextProvider>,
-    section,
-    () => SizeSelector.initSizeSelector((size) => sizemeStore.dispatch(selectSize(size, false)))
+    </I18nextProvider>
   );
+
+  SizeSelector.initSizeSelector((size) => sizemeStore.dispatch(selectSize(size, false)));
 }
