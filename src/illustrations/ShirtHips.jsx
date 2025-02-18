ecd1276647c431037d335c93edd34d40406328c0
Ville Könönen <ville@nomasi.com>
2025-02-18 21:17:07 +0200

    Illustrations: fixed shirt hips svg height

--- src/illustrations/ShirtHips.jsx
+++ src/illustrations/ShirtHips.jsx
@@ -11,7 +11,7 @@ const ShirtHips = (props) => {
       version="1.1"
       xmlns="http://www.w3.org/2000/svg"
       className="shirtHips"
-      viewBox="158 290 237.5 210"
+      viewBox="128 290 262.5 210"
       preserveAspectRatio="xMidYMin meet"
     >
       <defs>
